1f2260222ee46bc8825e4606b7a6cb3c4e609645
PocaCaop <oscargomezsedas@gmail.com>
2022-12-02 22:10:32 +0100

    ver 1.0

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -11,10 +11,11 @@ public class NumberGuessing {
 
         System.out.println("Hello and welcome to the number guessing game!\n You have 3 chances to guess the number, " +
                 "if you guess it you win, if you don't you lose!\n GOOD LUCK!");
+       //Game
         do {
-            System.out.println(cpuChoice);
             System.out.println("Guess a number from 1-10, you have " + chances + " chances left");
             correctType = scanner.hasNextInt();
+            //Error Control
             if (!correctType){
                 System.out.println("ERROR: You have to type a whole number");
                 scanner.nextLine();
@@ -23,6 +24,19 @@ public class NumberGuessing {
                 if (userChoice > 10 || userChoice < 1){
                     System.out.println("ERROR: You have to type a number between 1-10");
                     correctType = false;
+                }else {
+                    //Comparing cpuChoice with userChoice
+                    if (cpuChoice == userChoice){ //Win
+                        System.out.println("HOORAY You guessed it the number was " + cpuChoice);
+                    }else {
+                        chances--;
+                        if (chances == 0){ //Lose
+                            System.out.println("You have 0 chances left! See you next time!");
+                        }else {//Loses 1 chance and tries again
+                            System.out.println("AW! You missed, you have now " + chances + " chances left");
+                            correctType = false;
+                        }
+                    }
                 }
             }
         }while (!correctType);
